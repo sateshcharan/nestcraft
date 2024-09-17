11cde11991634ad2f412e09be8c6d801034f4a2e
unknown <sateshcharans@gmail.com>
2024-09-17 11:08:12 +0530

    backend setup

--- backend/main.js
+++ backend/main.js
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import { configDotenv } from "dotenv";
 import express from "express";
 configDotenv();
@@ -6,7 +7,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  exec("python scripts/main.py", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    res.send(stdout);
+  });
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
